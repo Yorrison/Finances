941f30a334d45c88d0bf47a70c05ecea010bb1c6
Yorrison <felipeyorrison25@gmail.com>
2024-11-17 16:47:08 -0300

    chore: add tailwindcsss prettier pluguin

--- finance/app/page.tsx
+++ finance/app/page.tsx
@@ -1,5 +1,8 @@
-const Home = () =>{
-  return <h1>Home</h1>
+const Home = () => {
+  return <div className="flex intems-center justify-center w-screen">
+    <h1 className="text-red-500">Home</h1>
+  </div>
+   
 }
 
 export default Home;
